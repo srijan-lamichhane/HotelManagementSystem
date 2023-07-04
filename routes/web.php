b10cd933ca862dec71d8f58d723344b1f8bdc511
srijan-lamichhane <shreejanlamichhane14b2r@gmail.com>
2023-07-04 07:23:04 +0545

    addbooking section.

--- routes/web.php
+++ routes/web.php
@@ -44,4 +44,5 @@ Route::get('/edit_profile',[AdminController::class,'edit_profile']);
 
 Route::get('form/allbooking',[BookingController::class,'allbooking']);
 Route::get('form/editbooking',[BookingController::class,'editbooking']);
+Route::get('form/addbooking',[BookingController::class,'addbooking']);
 
